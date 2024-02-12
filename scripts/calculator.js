c83f624f58710d3d5dcd907bbe40d987eba5b0f1
JanneKiianen <janne.kiianen@student.lab.fi>
2024-02-12 14:37:55 +0200

    Mobile testing

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,4 +1,45 @@
-window.onload() = ChooseNumbers(){
-
-    
+window.onload = function RandomNumbers()
+{
+    number1 = Math.floor(Math.random() * 10) + 1    
+    number2 = Math.floor(Math.random() * 10) + 1
+    document.getElementById("numberone").innerHTML = number1;
+    document.getElementById("numbertwo").innerHTML = number2;
 }
+
+
+function DecreaseFirstNumber()
+{
+    number1--
+    document.getElementById("numberone").innerHTML = number1;
+}
+function IncreaseFirstNumber()
+{
+    number1++
+    document.getElementById("numberone").innerHTML = number1;    
+}
+function DecreaseSecondNumber()
+{
+    number2--
+    document.getElementById("numbertwo").innerHTML = number2;
+}
+function IncreaseSecondNumber()
+{
+    number2++
+    document.getElementById("numbertwo").innerHTML = number2;
+}
+
+function CalculateStuff()
+{
+    const select = document.getElementById("mathSelect");
+    mathstuff = select.options[select.selectedIndex].value;
+    if (mathstuff == "+") {answer = parseInt(number1) + parseInt(number2)}
+    if (mathstuff == "-") {answer = parseInt(number1) - parseInt(number2)}
+    if (mathstuff == "/") {answer = parseInt(number1) / parseInt(number2)}
+    if (mathstuff == "*") {answer = parseInt(number1) * parseInt(number2)}
+    answer = answer.toFixed(2);
+    document.getElementById("result").innerHTML = answer;
+    document.getElementById("SuperButton").innerHTML = number1 + mathstuff + number2;
+    let calculation = "<p>" + number1 + mathstuff + number2 + "=" + answer + "</p>"
+    document.getElementById("Past_calculations").insertAdjacentHTML("afterend", calculation);
+}
+
