810cce3e5035960b67805f1d6666eed12bf71c9b
Janne Kiianen <Janne.Kiianen@student.lab.fi>
2024-02-22 20:59:11 +0200

    Dice

---
+++ D20test/Dice.js
@@ -0,0 +1,20 @@
+let i = 720
+
+function ROLL()
+{
+    $('#spinnable').animate(
+      { deg: i },
+      {
+        duration: 1200,
+        step: function(now) {
+          $(this).css({ transform: 'rotate(' + now  + 'deg)'});
+        }  
+      }
+    );
+    i = i + 720;
+    setTimeout(() => { number1 = Math.floor(Math.random() * 20) + 1; }, 1000); 
+    setTimeout(() => { document.getElementById('NumberHere').innerHTML = number1; }, 1000);
+    setTimeout(() => { $("#NumberHere").fadeIn();}, 1300);
+    
+ 
+};
