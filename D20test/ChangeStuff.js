d9ea22dbd6d989f2321e393f55bb0a7fd0293ceb
JanneKiianen <janne.kiianen@student.lab.fi>
2024-02-23 12:50:56 +0200

    More dice

--- D20test/ChangeStuff.js
+++ D20test/ChangeStuff.js
@@ -1,24 +1,34 @@
+window.onload = function()
+{
+    if (localStorage.hasOwnProperty('Dice')){document.getElementById('DiceHere').src = localStorage.getItem('Dice');}   
+}
+
 function ChangeRed()
 {
     document.getElementById('DiceHere').src = 'Images/D20-red.png'
+    localStorage.setItem('Dice', 'Images/D20-red.png')
 }
 
 function ChangeBlue()
 {
     document.getElementById('DiceHere').src = 'Images/D20-blue.png'
+    localStorage.setItem('Dice', 'Images/D20-blue.png')
 }
 
 function ChangeGold()
 {
     document.getElementById('DiceHere').src = 'Images/D20-gold.png'
+    localStorage.setItem('Dice', 'Images/D20-gold.png')
 }
 
 function ChangePurple()
 {
     document.getElementById('DiceHere').src = 'Images/D20-purple.png'
+    localStorage.setItem('Dice', 'Images/D20-purple.png')
 }
 
 function ChangeGreen()
 {
     document.getElementById('DiceHere').src = 'Images/D20-green.png'
+    localStorage.setItem('Dice', 'Images/D20-green.png')
 }
