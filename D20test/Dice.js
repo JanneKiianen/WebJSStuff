25d636f19e9183124d3451b00379829af96325c9
JanneKiianen <janne.kiianen@student.lab.fi>
2024-02-23 12:21:27 +0200

    message

--- D20test/Dice.js
+++ D20test/Dice.js
@@ -1,4 +1,5 @@
 //DO NOT EDIT!
+//fixed the roll
 
 let i = 720
 
@@ -13,7 +14,15 @@ function ROLL()
         }  
       }
     );
-    i = i + 720;
+    $('#spinnable').animate(
+      { deg: 0 },
+      {
+        duration: 0,
+        step: function(now) {
+          $(this).css({ transform: 'rotate(' + now  + 'deg)'});
+        }  
+      }
+    );
     setTimeout(() => { number1 = Math.floor(Math.random() * 20) + 1; }, 1000); 
     setTimeout(() => { document.getElementById('NumberHere').innerHTML = number1; }, 1000);
     setTimeout(() => { $("#NumberHere").fadeIn();}, 1300);
