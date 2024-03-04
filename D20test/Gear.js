ffa01805bf87132513d59bfbd53023c802e1ee85
JanneKiianen <janne.kiianen@student.lab.fi>
2024-03-04 13:24:53 +0200

    LectureMarch

---
+++ D20test/Gear.js
@@ -0,0 +1,78 @@
+
+    let Weapons = [
+        {
+            "Class": "Sword",
+            "Damage": 50,
+            "Weight": 5,
+            "Onehanded": "Yes",
+            "Image": "Placeholder"
+        },
+        {
+            "Class": "Mace",
+            "Damage": 60,
+            "Weight": 10,
+            "Onehanded": "Yes",
+            "Image": "Placeholder"
+        },
+        {
+            "Class": "Warhammer",
+            "Damage": 100,
+            "Weight": 50,
+            "Onehanded": "No",
+            "Image": "Placeholder"
+        },
+        {
+            "Class": "Staff",
+            "Damage": 90,
+            "Weight": 35,
+            "Onehanded": "No",
+            "Image": "Placeholder"
+        }
+    ]
+
+
+
+    let = Armor = [
+        {
+            "Class": "Heavy armor",
+            "Resistance": 30,
+            "Weight": 50,
+            "Metal": "Yes",
+            "Image": "Placeholder"
+        },
+        {
+            "Class": "Robes",
+            "Resistance": 10,
+            "Weight": 10,
+            "Metal": "No",
+            "Image": "Placeholder"
+        },
+        {
+            "Class": "Leather armor",
+            "Resistance": 20,
+            "Weight": 20,
+            "Metal": "No",
+            "Image": "Placeholder"
+        }
+    ]
+
+
+
+    let Trinkets = [
+        {
+            "Class": "Amulet",
+            "Resistance": 5,
+            "Crit%" : 25,
+            "Weight": 5,
+            "Metal": "Yes",
+            "Image": "Placeholder"
+        },
+        {
+            "Class": "Ring",
+            "Resistance": 1,
+            "Crit%" : 5,
+            "Weight": 1,
+            "Metal": "Yes",
+            "Image": "Placeholder"
+        }
+    ]
